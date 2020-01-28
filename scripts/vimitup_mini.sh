09b57b70a5e7629b714877b83b5f1035acf3d74d
Michael Lingelbach <m.j.lbach@gmail.com>
2020-01-28 16:00:26 -0500

    Fix

--- scripts/vimitup_mini.sh
+++ scripts/vimitup_mini.sh
@@ -24,7 +24,7 @@ install_apt_packages(){
 
 
 install_dependencies(){
-    curl -fLo https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb
+    curl -fLO https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb
     sudo dpkg -i fd_7.4.0_amd64.deb
     rm -rf fd_7.4.0_amd64.deb 
     curl -fLO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
